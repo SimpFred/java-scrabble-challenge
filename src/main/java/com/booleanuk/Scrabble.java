34435d01fb674c9a56e613241f8f70027a2b79cb
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-08-13 14:04:26 +0200

    Done with the core.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,43 @@
 package com.booleanuk;
 
-public class Scrabble {
-    public Scrabble(String word) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Scrabble {
+    String word;
+    Map<Character, Integer> scoreMap = createScoreMap();
+
+    public Scrabble(String word) {
+        this.word = word.toUpperCase();
     }
 
     public int score() {
-        return -1;
+        int score = 0;
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                score += scoreMap.get(c);
+            }
+        }
+        return score;
     }
 
+    private Map<Character, Integer> createScoreMap() {
+        Map<Character, Integer> map = new HashMap<>();
+
+        addScores(map, 1, "AEIOULNRST");
+        addScores(map, 2, "DG");
+        addScores(map, 3, "BCMP");
+        addScores(map, 4, "FHVWY");
+        addScores(map, 5, "K");
+        addScores(map, 8, "JX");
+        addScores(map, 10, "QZ");
+
+        return map;
+    }
+
+    private void addScores(Map<Character, Integer> map, int score, String letters) {
+        for (char letter : letters.toCharArray()) {
+            map.put(letter, score);
+        }
+    }
 }
