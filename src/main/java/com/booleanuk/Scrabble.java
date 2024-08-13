b5bb286b120a754118ccb00fe104a0966b664325
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-08-13 16:08:38 +0200

    First version working for the extension.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -6,6 +6,10 @@ import java.util.Map;
 public class Scrabble {
     String word;
     Map<Character, Integer> scoreMap = createScoreMap();
+    boolean isLetterDouble = false;
+    boolean isLetterTriple = false;
+    boolean isWordDouble = false;
+    boolean isWordTriple = false;
 
     public Scrabble(String word) {
         this.word = word.toUpperCase();
@@ -13,17 +17,77 @@ public class Scrabble {
 
     public int score() {
         int score = 0;
-        for (char c : word.toCharArray()) {
-            if (Character.isLetter(c)) {
-                score += scoreMap.get(c);
+
+        for (int i = 0; i < word.length(); i++) {
+            char current = word.charAt(i);
+
+
+            if (current == '{') {
+                if (word.charAt((i + 2)) == '}') {
+                    isLetterDouble = true;
+                } else if (i == 0 && word.charAt((word.length() - 1)) == '}' || i == 1 && word.charAt((word.length() - 2)) == '}') {
+                    isWordDouble = true;
+                } else {
+                    return 0;
+                }
+            } else if (current == '}') {
+                if (i < 2) {
+                    return 0;
+                }
+                else if (word.charAt((i - 2)) == '{') {
+                    isLetterDouble = false;
+
+                } else if (!isWordDouble) {
+                    return 0;
+                }
+
+            } else if (current == '[') {
+                if (word.charAt((i + 2)) == ']') {
+                    isLetterTriple = true;
+                } else if (i == 0 && word.charAt((word.length() - 1)) == ']' || i == 1 && word.charAt((word.length() - 2)) == ']') {
+                    isWordTriple = true;
+                } else {
+                    return 0;
+                }
+            } else if (current == ']') {
+                if (i < 2) {
+                    return 0;
+                }
+                else if (word.charAt((i - 2)) == '[') {
+                    isLetterTriple = false;
+                } else if (!isWordTriple) {
+                    return 0;
+                }
+            } else {
+                int letterScore = scoreMap.getOrDefault(current, -1);
+                if (letterScore == -1 ) {
+                    return 0;
+                }
+
+                if (isLetterDouble) {
+                    letterScore *= 2;
+                } else if (isLetterTriple) {
+                    letterScore *= 3;
+                }
+
+                score += letterScore;
             }
         }
+
+        if (isWordDouble) {
+            score *= 2;
+        }
+        if (isWordTriple) {
+            score *= 3;
+        }
+
         return score;
     }
 
     private Map<Character, Integer> createScoreMap() {
         Map<Character, Integer> map = new HashMap<>();
 
+        addScores(map, 0, "{[]}");
         addScores(map, 1, "AEIOULNRST");
         addScores(map, 2, "DG");
         addScores(map, 3, "BCMP");
